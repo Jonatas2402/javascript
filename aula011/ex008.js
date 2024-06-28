d914d23b13287b374892d60e5f9681a3a87d90c3
Jonatas2402 <jonatas.silva98@outlook.com.br>
2024-06-28 15:09:19 -0300

    condições 01

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+let vel = 60.3
+console.log(`A velocida de do seu carro é ${vel}Km/h`);
+if(vel > 60) { //condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}  
+console.log(`Dirija sempre usando cinto de segurança!`);
+
