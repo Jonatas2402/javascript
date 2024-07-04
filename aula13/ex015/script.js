47958051535aec554088823d09e88ce5271ad835
Jonatas2402 <jonatas.silva98@outlook.com.br>
2024-07-04 18:46:15 -0300

    pj 2

--- aula13/ex015/script.js
+++ aula13/ex015/script.js
@@ -1,1 +1,12 @@
-alert
+function verificar() {
+    let data = new Date()
+    let ano = data.getFullYear()
+    let fano = document.getElementById('txtano')
+    let res = document.querySelector('div#res')
+    if(fano.value.length == 0 || fano.value > ano) {
+       alert('verifique os dados e tente novamente') 
+    }else {
+        alert('tudo ok')
+    }
+
+}
