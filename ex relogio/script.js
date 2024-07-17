6cb71a79a093f941f580b43be45dcbb732b2cedf
jonatas <jonatas_dev@hotmail.com>
2024-07-17 18:15:32 -0300

    inicio relógio

--- ex relogio/script.js
+++ ex relogio/script.js
@@ -1,23 +1,22 @@
-const secondHand = document.querySelector('.second-hand');
-const minsHand = document.querySelector('.min-hand')
-const hourHand = document.querySelector('.hour-hand')
+const hourHnad = document.querySelector(".hand.hours");
+const minutesHand = document.querySelector(".hand.minutes");
+const secondsHand = document.querySelector(".hand.seconds");
 
-function setDate() {
-    const now = new Date();
-
-    const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60)* 360) + 90;
-    secondsHand.style.transform = `rotate (${secondsDegrees}deg)`;
-
-    const mins = now.getMinutes();
-    const minsDegrees = ((mins / 60) * 360) + ((seconds/60) *6) + 90;
-    minsHand.style.transform = `rotate (${minsDegrees}deg)`;
-
-    const hour = now.getHours();
-    const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
-    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+function setRotation(element, rotationPercentage) {
+    element.style.setProperty("--rotation", rotationPercentage * 360);
 }
 
-setInterval(setDate, 1000);
+function setClock() {
+  const currentDate = new Date();
 
-setDate();
+  const secondsPercentage = currentDate.getSeconds() / 60;
+  const minutesPercetage = currentDate.getMinutes() / 60;
+  const hoursPercentage = currentDate.getHours() / 12;
+
+  setRotation(secondsHand,secondsPercentage);
+  setRotation(minutesHand, minutesPercetage);
+  setRotation(hoursHand, hoursPercentage);
+};
+setClock();
+
+setInterval(setClock, 1000);
